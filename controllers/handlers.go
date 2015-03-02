1e7f4f1316ac6e7f6093a109e758959c94c2cab4
Fredrik Berntsson <fredrik.kona@gmail.com>
2015-03-03 00:18:42 +0100

    trololol

--- controllers/handlers.go
+++ controllers/handlers.go
@@ -8,24 +8,23 @@ import (
 	"net/http"
 )
 
-
 func Exercises(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	log.Println("Get exercises called by userid: " + id + " (" + r.RemoteAddr + ")")
 	exercises := models.GetExercises(id)
-    json.NewEncoder(w).Encode(exercises)
+	json.NewEncoder(w).Encode(exercises)
 }
 
-func Workouts(w http.ResponseWriter, r * http.Request) {
-    id := mux.Vars(r)["id"]
-    log.Println("Get workouts called by userid: " + id + " (" + r.RemoteAddr + ")")
-    workouts := models.GetWorkouts(id)
-    json.NewEncoder(w).Encode(workouts)
+func Workouts(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	log.Println("Get workouts called by userid: " + id + " (" + r.RemoteAddr + ")")
+	workouts := models.GetWorkouts(id)
+	json.NewEncoder(w).Encode(workouts)
 }
 
-func Schedules(w http.ResponseWriter, r * http.Request) {
-    id := mux.Vars(r)["id"]
-    log.Println("Get schedules called by userid: " + id + " (" + r.RemoteAddr + ")")
-    schedules := models.GetSchedules(id)
-    json.NewEncoder(w).Encode(schedules)
+func Schedules(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	log.Println("Get schedules called by userid: " + id + " (" + r.RemoteAddr + ")")
+	schedules := models.GetSchedules(id)
+	json.NewEncoder(w).Encode(schedules)
 }
--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-    Id string `json:"user"`
-    Name string `json:"name"`
-    Lastname string `json:"lastname"`
-    Email string `json:"email"`
-    Password string `json:"password"`
+	Id       string `json:"user"`
+	Name     string `json:"name"`
+	Lastname string `json:"lastname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
